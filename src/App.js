fbdf931d94d806ea0d06699ba5b44e32735eb7bc
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-06-16 11:14:54 -0700

    removed form from home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
 import Home from './Home'
-import 'bootstrap/dist/css/bootstrap.min.css';
-=======
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import Landing from "./components/Landing"
 import Signup from "./components/Form"
 import Jumbo from "./components/Jumbo"
@@ -11,34 +9,30 @@ import Logo from "./components/Logo"
 import CustomNavbar from "./components/CustomNavbar"
 import Fig from './components/Figures';
 
->>>>>>> c28392c2190dd73451e3e34ff99a62638acf84c7
 
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Home />
-=======
       <CustomNavbar />
       <div className="">
         <style>{'body { background-color: #edd7cc; }'}</style>
         <Landing />
       </div>
       <div className="fig-container">
-        <Fig 
+        <Fig
           width={300}
           height={300}
           image={"todo.jpg"}
           caption={"Todo List"}
         />
-        <Fig 
+        <Fig
           width={300}
           height={300}
           image={"yoga.jpg"}
           caption={"Habit"}
         />
-        <Fig 
+        <Fig
           width={300}
           height={300}
           image={"happy.jpg"}
@@ -48,9 +42,8 @@ function App() {
 
 
       <Signup />
->>>>>>> c28392c2190dd73451e3e34ff99a62638acf84c7
     </div>
-    
+
   );
 }
 
