f2426971b025a35ab5fbc338ffeb0ba3cc0d859f
shaannessy25 <47993680+shaannessy25@users.noreply.github.com>
2020-06-16 04:46:59 -0700

    adding figures

---
+++ src/components/Figures.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Figure, FigureImage } from 'react-bootstrap';
+import './Figures.css';
+
+
+function Fig(props) {
+    const { width, height, image, caption } = props
+    return (
+    
+      <div>
+        <Figure>
+            <img 
+                width={width}
+                height={height}
+                alt="171x180"
+                src={`${process.env.PUBLIC_URL}images/${image}`}   
+            />
+            <Figure.Caption >
+                {caption}
+            </Figure.Caption>
+        </Figure>
+      </div>
+    
+    );
+  }
+  
+  export default Fig;
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import { Navbar, Jumbotron, Nav, Container } from 'react-bootstrap';
-import '../components/Landing.css'
+// import { Navbar, Jumbotron, Nav, Container } from 'react-bootstrap';
+import './Landing.css';
 
 
 function Landing() {
+
     return (
       <div>
-        <Navbar className="Navbar" expand="lg">
-            <Navbar.Brand href="#home">myLife</Navbar.Brand>
-            <Nav>
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">About</Nav.Link>
-                <div className="navlink">
-                    <Nav.Link>Login</Nav.Link>
-                </div> 
-            </Nav>
-        </Navbar>  
+        <div>
+          <header className="header" >
+            <img src={`${process.env.PUBLIC_URL}images/LandingImage.jpg`} alt="Logo" className="image" />
+            <div className="logo-box">
+              <img src={`${process.env.PUBLIC_URL}images/Logo.png`} alt="Logo" className="logo" />
+              
+            </div>
+          <div className="text-box">
+              <h1 className="heading-primary">
+                  <span className="heading-primary-main">myLyfe</span>
+              <span className="heading-primary-sub">Time To Level Up</span>
+              </h1>
+              <a href="" className="btn btn-white btn-animated">Start Your Journey</a>
+          </div>
+          </header>
+        </div>
       </div>
     
     );
