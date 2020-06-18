588c17ea518f17b1e722ba66ea72564cf567b2fb
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-06-17 20:44:37 -0700

    Routes for login and register working

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -4,19 +4,19 @@ import "./CustomNavbar.css"
 
 
 
-function CustomNavbar(){
+function CustomNavbar() {
     return (
         <div>
             <Navbar className="Navbar" expand="lg">
-                <Navbar.Brand href="#home"><img src={`${process.env.PUBLIC_URL}images/Logo.png`} alt="Logo" className="brand"/></Navbar.Brand>
+                <Navbar.Brand href="#home"><img src={`${process.env.PUBLIC_URL}images/Logo.png`} alt="Logo" className="brand" /></Navbar.Brand>
                 <Nav>
                     <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">About</Nav.Link>
+                    <Nav.Link href="/login">About</Nav.Link>
                     <div className="navlink">
                         <Nav.Link>Login</Nav.Link>
-                    </div> 
+                    </div>
                 </Nav>
-            </Navbar>  
+            </Navbar>
         </div>
     )
 }
