3b7afe2448147a3531f117e77420ce83526c78d4
Alanna <alanna.noguchi@students.makeschool.com>
2020-06-18 19:26:22 -0700

    hooks index.js

---
+++ src/hooks/index.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import { firebase } from '../../firebase';
+
+const collatedTasks = () => {};
+
+export const useTasks = selectedProject => {
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        let unsubscribe = firebase
+        .firestore()
+        .collection('tasks')
+        .where('userId', '==', 'userkeyid');
+        
+        unsubscribe = 
+            selectedProject && !collatedTasksExist(selectedProject) 
+            ? (unsubscribe = unsubscribe.where('projectId', '==', selectedProject))
+            : selectedProject === 'Today'
+            ? (unsubscribe = unsubscribe.where(
+                'date',
+                '==',
+                moment().format('DD/MM/YYYY')
+                ))
+            : selectedProject === 'INBOX' || selectedProject === 0
+            ? (unsubscribe = unsubscribe.where('date', '==', ''))
+            : unsubscribe;
+        }, [selectedProject]);
+}
