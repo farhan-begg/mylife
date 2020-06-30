92a153be8d5848b756c934159d420311fa18292a
shaannessy25 <47993680+shaannessy25@users.noreply.github.com>
2020-06-30 11:30:51 -0700

    old landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,77 +1,32 @@
-import React, { useRef } from 'react';
+
+import React from 'react';
 // import { Navbar, Jumbotron, Nav, Container } from 'react-bootstrap';
-// import './Landing.css';
-// import './Slicebox.css';
-// import './Landing2.css';
-
-export default function Landing() {
-  const wrapper = useRef(null)
-  const sbSlider = useRef(null)
-  return (
-		<div className="container"> 
-
-			<h1 className="title">My Lyfe <span>Time To Level Up</span></h1>
-			<div ref={wrapper} className="wrapper">
-
-				<ul ref={sbSlider} id="sb-slider" className="sb-slider">
-					<li>
-						<a href="/register" target="_blank"><img src="images/1.jpg" alt="image1"/></a>
-						<div className="sb-description">
-							<h3>Register</h3>
-						</div>
-					</li>
-					<li>
-						<a href="/todo" target="_blank"><img src={`${process.env.PUBLIC_URL}images/2.jpg`} alt="image2"/></a>
-						<div className="sb-description">
-							<h3>Todo List</h3>
-						</div>
-					</li>
-					<li>
-						<a href="/login" target="_blank"><img src={`${process.env.PUBLIC_URL}images/3.jpg`} alt="image1"/></a>
-						<div className="sb-description">
-							<h3>Habit Tracker</h3>
-						</div>
-					</li>
-					<li>
-						<a href="/" target="_blank"><img src={`${process.env.PUBLIC_URL}images/4.jpg`} alt="image1"/></a>
-						<div className="sb-description">
-							<h3>Progress Tracker</h3>
-						</div>
-					</li>
-					<li>
-						<a href="/" target="_blank"><img src={`${process.env.PUBLIC_URL}images/5.jpg`} alt="image1"/></a>
-						<div className="sb-description">
-							<h3>Rewards</h3>
-						</div>
-					</li>
-					<li>
-						<a href="/" target="_blank"><img src={`${process.env.PUBLIC_URL}images/6.jpg`} alt="image1"/></a>
-						<div className="sb-description">
-							<h3>Groundbreaking Artist</h3>
-						</div>
-					</li>
-					<li>
-						<a href="/" target="_blank"><img src={`${process.env.PUBLIC_URL}images/7.jpg`} alt="image1"/></a>
-						<div className="sb-description">
-							<h3>Learn More</h3>
-						</div>
-					</li>
-				</ul>
-
-				<div id="shadow" className="shadow"></div>
-
-				<div id="nav-arrows" className="nav-arrows">
-					<a href="#">Next</a>
-					<a href="#">Previous</a>
-				</div>
-
-			</div>
-
-		</div>
-
-  );
-}
+import './Landing.css';
 
 
+function Landing() {
 
-
+    return (
+      <div>
+        <div>
+          <header className="header" >
+            <img src={`${process.env.PUBLIC_URL}images/LandingImage.jpg`} alt="Logo" className="image" />
+            <div className="logo-box">
+              {/* <img src={`${process.env.PUBLIC_URL}images/Logo.png`} alt="Logo" className="logo" /> */}
+              
+            </div>
+          <div className="text-box">
+              <h1 className="heading-primary">
+                  <span className="heading-primary-main">myLyfe</span>
+              <span className="heading-primary-sub">Time To Level Up</span>
+              </h1>
+              <a href="" className="btn btn-white btn-animated">Start Your Journey</a>
+          </div>
+          </header>
+        </div>
+      </div>
+    
+    );
+  }
+  
+  export default Landing;
