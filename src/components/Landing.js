e01b496963e1d331d1b5d92d2793c387bf9db3a1
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-06-30 11:56:01 -0700

    fixed form style error

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -9,7 +9,7 @@ function Landing() {
     return (
       <div>
         <div>
-          <header className="header" >
+          <header className="header-1" >
             <img src={`${process.env.PUBLIC_URL}images/LandingImage.jpg`} alt="Logo" className="image" />
             <div className="logo-box">
               {/* <img src={`${process.env.PUBLIC_URL}images/Logo.png`} alt="Logo" className="logo" /> */}
