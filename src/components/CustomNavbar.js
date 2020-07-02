77ecb0f44e2e78b4441b9764df5661b86de2dc47
Alanna <alanna.noguchi@students.makeschool.com>
2020-07-01 22:55:08 -0700

    removed dropdown from navbar

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -15,9 +15,9 @@ function CustomNavbar() {
             <NavItem icon={<PlusIcon />} />
             <NavItem icon= {<NoteIcon />} />
     
-            <NavItem icon= {<CaretIcon />}>
+            {/* <NavItem icon= {<CaretIcon />}>
               <DropdownMenu></DropdownMenu>
-            </NavItem>
+            </NavItem> */}
         </Navbar>
       );
     }
@@ -36,7 +36,7 @@ function NavItem(props) {
     
     return (
         <li className='nav-item'>
-            <a href='/register' className='icon-button' onClick={() => setOpen(!open)}>
+            <a href='' className='icon-button' onClick={() => setOpen(!open)}>
                 {props.icon}
             </a>
 
@@ -45,69 +45,64 @@ function NavItem(props) {
     );
 }
 
-function DropdownMenu() {
+// function DropdownMenu() {
 
-    const [activeMenu, setActiveMenu] = useState('main');
-    const [menuHeight, setMenuHeight] = useState(null);
+//     const [activeMenu, setActiveMenu] = useState('main');
+//     const [menuHeight, setMenuHeight] = useState(null);
 
-    function calcHeight(el) {
-    const height = el.offsetHeight;
-    setMenuHeight(height)
-    }
+//     function calcHeight(el) {
+//         const height = el.offsetHeight;
+//         setMenuHeight(height)
+//     }
 
-    function DropdownItem(props) {
-    return (
-        <a href='#' className='menu-item' onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
-        <span className='icon-button'>{props.leftIcon}</span>
+//     function DropdownItem(props) {
+//         return (
+//             <a href='#' className='menu-item' onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
+//                 <span className='icon-button'>{props.leftIcon}</span>
 
-        {props.children}
+//                 {props.children}
 
-        <span className='icon-right'>{props.rightIcon}</span>
-        </a>
-    );
-    }
-    return (
-    <div className='dropdown' style={{ height: menuHeight}}>
+//                 <span className='icon-right'>{props.rightIcon}</span>
+//             </a>
+//         );
+//     }
+//     return (
+//         <div className='dropdown' style={{ height: menuHeight}}>
 
-        <CSSTransition 
-        in={activeMenu === 'main'} 
-        unmountOnExit 
-        timeout={500}
-        className='menu-primary'
-        onEnter={calcHeight}
-        >
-            <div className='menu'>
-            <DropdownItem>My Profile</DropdownItem>
-            <DropdownItem
-            leftIcon={<CogIcon />}
-            rightIcon={<ChevronIcon />}
-            goToMenu='settings'>
-            Settings
-            </DropdownItem>
-            </div>
-        </CSSTransition> 
+//             <CSSTransition 
+//                 in={activeMenu === 'main'} 
+//                 unmountOnExit 
+//                 timeout={500}
+//                 className='menu-primary'
+//                 onEnter={calcHeight}>
+//                 <div className='menu'>
+//                     <DropdownItem>My Profile</DropdownItem>
+//                     <DropdownItem
+//                         leftIcon={<CogIcon />}
+//                         rightIcon={<ChevronIcon />}
+//                         goToMenu='settings'>
+//                         Settings
+//                     </DropdownItem>
+//                 </div>
+//         </CSSTransition> 
 
-        <CSSTransition 
-        in={activeMenu === 'settings'} 
-        unmountOnExit 
-        timeout={500}
-        className='menu-secondary'
-        >
-            <div className='menu'>
-            <DropdownItem leftIcon={<ArrowIcon />} goToMenu='main' />
-            <DropdownItem>Settings</DropdownItem>
-            <DropdownItem>Settings</DropdownItem>
-            <DropdownItem>Settings</DropdownItem>
-            <DropdownItem>Settings</DropdownItem>
-            <DropdownItem>Settings</DropdownItem>
-            <DropdownItem>Settings</DropdownItem>
-            <DropdownItem>Settings</DropdownItem>
-            <DropdownItem>Settings</DropdownItem>
-            <DropdownItem>Settings</DropdownItem>
-            </div>
-        </CSSTransition> 
-    </div>
-    );
-    }
+//         <CSSTransition 
+//             in={activeMenu === 'settings'} 
+//             unmountOnExit 
+//             timeout={500}
+//             className='menu-secondary'>
+//             <div className="menu">
+//                 <DropdownItem goToMenu="main" leftIcon={<ArrowIcon />}>
+//                     <h2>Animals</h2>
+//                 </DropdownItem>
+//                 <DropdownItem leftIcon="🦘">Kangaroo</DropdownItem>
+//                 <DropdownItem leftIcon="🐸">Frog</DropdownItem>
+//                 <DropdownItem leftIcon="🦋">Horse?</DropdownItem>
+//                 <DropdownItem leftIcon="🦔">Hedgehog</DropdownItem>
+//             </div>
+//         </CSSTransition> 
+//     </div>
+//     );
+// }
 
 export default CustomNavbar;
