16af626bbad17a129d2d8ce97e2ddce2003b10f8
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-07-04 17:07:16 -0700

    added loading symbol

--- src/login/Auth.js
+++ src/login/Auth.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import firebase from "../firebase.js";
+import './loading.css'
 
 
 export const AuthContext = React.createContext();
@@ -17,9 +18,15 @@ export const AuthProvider = ({ children }) => {
 
     if (pending) {
         return <div className="loading">
+            <svg class="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+                <circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+            </svg>
 
-            <>Loading...</>
         </div>
+
+
+
+
     }
 
     return (
