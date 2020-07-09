e3b5ada4b96fe1f3c1fc65fdf2c299bd21fe7574
Alanna <alanna.noguchi@students.makeschool.com>
2020-07-09 10:50:05 -0700

    changed features section

--- src/components/Features.js
+++ src/components/Features.js
@@ -20,16 +20,16 @@ function Features() {
                                 <p>Keep a list of the things you need to do.</p> 
                             </div>
                         </div>
-                        <div className="flip-card-back">
+                        {/* <div className="flip-card-back">
                             <h1>Monday</h1> 
                             <p>1. Morning Run</p> 
                             <p>2. Feed dogs</p>
                             <p>3. Call Mom</p>
                             <p>4. Book flights to Hawaii</p>
-                        </div>  
+                        </div>   */}
                     </div>
                 </div>
-                <div className="card-front">
+                <div className="card-frontt">
                     <div className="flip-card-inner">
                         <div className="flip-card-front">
                             <img className="card-image" src="https://images.unsplash.com/photo-1543286386-2e659306cd6c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="Avatar"/>
@@ -38,14 +38,14 @@ function Features() {
                                 <p>Track your progression with each habit.</p> 
                             </div>  
                         </div>
-                        <div className="flip-card-back">
+                        {/* <div className="flip-card-back">
                             <h1>Name of Habit: Biking</h1> 
                             <p>Amount: 10</p> 
                             <p>Progress: 4/10</p>
-                        </div>  
+                        </div>   */}
                     </div>
                 </div>
-            <div className="card-front">
+            <div className="card-frontt">
                 <div className="flip-card-inner">
                     <div className="flip-card-front">
                         <img className="card-image" src="https://images.unsplash.com/photo-1535726685122-73b80565bf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="Avatar"/>
@@ -54,11 +54,11 @@ function Features() {
                             <p>Made by you, for you.</p> 
                         </div>
                     </div>
-                <div className="flip-card-back">
+                {/* <div className="flip-card-back">
                     <h1>John Doe</h1> 
                     <p>Architect & Engineer</p> 
                     <p>We love that guy</p>
-                </div>  
+                </div>   */}
                 </div>          
             </div>  
         </div>
